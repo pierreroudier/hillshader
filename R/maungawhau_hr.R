267b90f4e98ca530d0318e99c57d4a8b2351686f
Pierre Roudier <pierre.roudier@gmail.com>
2021-06-03 16:25:50 +1200

    added LiDAR dataset

---
+++ R/maungawhau_hr.R
@@ -0,0 +1,8 @@
+#' LiDAR Elevation Raster for Maungawhau in Tāmaki Mākaurau/Auckland
+#'
+#' Elevation data as a raster for Maungawhau, a volcano located in Tāmaki Mākaurau/Auckland.
+#'
+#' @format A `RasterLayer` with 860 rows, 600 columns, and 1 band with the elevation data at a 1 m resolution. The data is projected in New Zealand Map Grid (NZMG, EPSG:27200).
+#'
+#' @source Elevation data from LINZ Data Service: \url{https://data.linz.govt.nz/layer/53405-auckland-lidar-1m-dem-2013/}
+"maungawhau_hr"
---
+++ data-raw/maungawhau_hr.R
@@ -0,0 +1,20 @@
+library(raster)
+
+data("maungawhau")
+
+# LiDAR data downloaded from LINZ data service
+# at https://data.linz.govt.nz/layer/53405-auckland-lidar-1m-dem-2013/
+#
+# - unzip downloaded archive
+# - create VRT: gdalbuildvrt akl-lidar.vrt *.tif
+# - reproject and convert to geoTIFF:
+#  gdalwarp akl-lidar.vrt akl-lidar.tif -t_srs EPSG:27200 -te 2667405 6478705 2668005 6479565
+# - crop to `volcano` footprint:
+r <- raster("./data-raw/akl-lidar.tif")
+
+gf <- focalWeight(r, 2, "Gauss")
+rg <- focal(r, w = gf)
+
+maungawhau_hr <- rg
+
+usethis::use_data(maungawhau_hr, overwrite = TRUE)
