16073a7bca5aaeb7a0d15e5e870c8f2dcf025871
Pierre Roudier <pierre.roudier@gmail.com>
2021-06-08 11:26:26 +1200

    corrected maungawhau orientation

--- data-raw/maungawhau.R
+++ data-raw/maungawhau.R
@@ -1,8 +1,9 @@
 ## code to prepare `maungawhau` dataset goes here
 mw <- datasets::volcano
-mw <- mw[nrow(mw):1,]
+mw <- mw[nrow(mw):1, ncol(mw):1]
 y <- seq(from = 6478705, length.out = 87, by = 10)
 x <- seq(from = 2667405, length.out = 61, by = 10)
+
 maungawhau <- raster::raster(
   mw,
   xmn = min(x), xmx = max(x),
